952ef4c810e45538f7a87795c111510168292b3b
gyanendrapc <dev.gyanendrapc@gmail.com>
2023-04-11 21:58:48 +0545

    Builder method

---
+++ CreationalPatterns/BuilderMethod/ConcreteBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_CS.CreationalPatterns.BuilderMethod
+{
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+        public void BuildPartA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildPartB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildPartC()
+        {
+            throw new NotImplementedException();
+        }
+        public Product GetProduct()
+        {
+            Product result = this._product;
+            this.Reset();
+            return result;
+        }
+    }
+}
