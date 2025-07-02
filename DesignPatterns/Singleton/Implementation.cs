5587bd4725526a565ec8a0f30755e0e15343af2e
Gyanendra Chaudhary <gyanendra.chaudhary@avishkaram.com>
2025-07-02 23:51:53 +0545

    implementing creational/singleton design pattern

---
+++ DesignPatterns/Singleton/Implementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger? _instance;
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+        protected Logger()
+        {
+
+        }
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using Singleton;
+
+Console.Title= "Singleton Pattern Example";
+
+//var logger = Logger();
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Both instances are the same.");
+}
+else
+{
+    Console.WriteLine("Instances are different.");
+}
+
+instance1.Log($"Message from {nameof(instance1)}");
+instance1.Log($"Message from {nameof(instance2)}");
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
